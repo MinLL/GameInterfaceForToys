8c74396022724481b9b5cb42fef9b92403493d5c
user@example.test <user@example.test>
2022-07-21 21:32:26 +1000

    Add KizunaInterface

--- SkyrimToyInterface.py
+++ SkyrimToyInterface.py
@@ -1,14 +1,17 @@
-import os
+import asyncio
+import bluetooth
+import datetime
 import io
-import time
+import os
+import random
 import re
 import requests
-import random
-import sys
-import asyncio
+import serial
+import time
 from buttplug.client import (ButtplugClientWebsocketConnector, ButtplugClient,
-                             ButtplugClientDevice, ButtplugClientConnectorError)
+                             ButtplugClientConnectorError)
 from dg_interface import CoyoteInterface
+from serial.tools.list_ports import (comports)
 import types
 from constants import *
 from settings import *
@@ -269,6 +272,63 @@ class ButtplugInterface(object):
             await device.send_stop_device_cmd()
 
 
+class KizunaInterface(object):
+    KIZUNA_NAME = "KIZUNA SMART"
+    UPDATE_DELAY = datetime.timedelta(seconds = 0.4)
+
+    def __init__(self):
+        self.kizuna_serial_port = None
+        self.last_updated = datetime.datetime.utcnow()
+        self.stop_time = None
+
+    def shutdown(self):
+        pass 
+    
+    def connect(self):
+        self.com_port = self._open_serial_port()
+
+    def check_in(self):
+        now = datetime.datetime.utcnow()
+        if (self.stop_time is not None and now >= self.stop_time):
+            self.stop_time = None
+            self.stop()
+        
+    def vibrate(self, duration, strength):
+        now = datetime.datetime.utcnow()
+        # Delay to avoid overwhelming the motor with sudden changes
+        if (now - self.last_updated >= self.UPDATE_DELAY):
+            self._write_speed(strength // 10)
+            self.stop_time = now + datetime.timedelta(seconds = duration)
+            # Fresh time to ensure the delay isn't shortened by scheduler delays
+            self.last_updated = datetime.datetime.utcnow()
+
+    def stop(self):
+        self._write_speed(0)
+
+    def _open_serial_port():
+        nearby_devices = bluetooth.discover_devices(lookup_names = True, flush_cache = True, duration = 1)
+        kizuna_info = [i for i in nearby_devices if i[1] == "KIZUNA SMART"]
+        if len(kizuna_info) == 1:
+            addr = kizuna_info[0][0]
+            addr_short = addr.replace(":", "")
+
+            com_ports = comports()
+            kizuna_comport = [p for p in com_ports if addr_short in p.hwid]
+
+            if len(kizuna_comport) == 1:
+                kizuna_serial = serial.Serial(kizuna_comport[0].device)
+                info("Connected to Kizuna Smart Controller on port {}".format(kizuna_comport[0].name))
+                return kizuna_serial
+        fail("Failed to find Kizuna Smart Controller")
+        return None
+
+    def _write_speed(self, speed):
+        if (speed >= 0 and speed <= 9):
+            self.kizuna_serial_port.write(bytes(str(speed) + "\r\n", "ASCII"))
+        else:
+            fail("Bad speed: " + speed)
+
+
 class ToyInterface(object):
     def shutdown(self):
         ret = []
@@ -288,6 +348,8 @@ class ToyInterface(object):
                                                  power_multiplier=7.68,
                                                  default_channel="a",
                                                  safe_mode=True)]  # See implementation for parameter details
+            elif toy == TOY_KIZUNA:
+                self.interface += [KizunaInterface()]
             else:
                 raise FatalException("Unsupported toy type!")
 
